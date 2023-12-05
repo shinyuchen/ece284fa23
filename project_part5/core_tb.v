1b16d7a6805575b31c4ec00f257d91be2a5b653a
shinyuchen <b08901114@ntu.edu.tw>
2023-12-05 10:04:21 -0800

    za warudo

--- project_part5/core_tb.v
+++ project_part5/core_tb.v
@@ -243,7 +243,7 @@ initial begin
 
 
     /////// 2. Kernel loading to PEs -> sequentially inject weight values into PEs from L0///////
-    for(t=0; t<row+col; t=t+1) begin // refer to W4S2 P.15
+    for(t=0; t<row+2*col; t=t+1) begin // refer to W4S2 P.15
       #0.5 
         clk = 1'b0;
         ififo_rd = 1'b1;
