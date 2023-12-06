0249d14ac0ccb3c058b5abbeb51f0ced78c8de6d
shinyuchen <b08901114@ntu.edu.tw>
2023-12-05 18:32:51 -0800

    za warudo

--- project/core_tb.v
+++ project/core_tb.v
@@ -289,19 +289,19 @@ initial begin
 
 
     /////// 2. Kernel loading to PEs -> sequentially inject weight values into PEs from L0///////
-    // #0.5 
-    //   clk = 1'b0;
-    //   ififo_rd = 1'b1;
-    //   if(kij == 0) begin
-    //     x_scan_file = $fscanf(x_file,"%32b", D_xmem); 
-    //     WEN_xmem = 0; 
-    //     CEN_xmem = 0; 
-    //     A_xmem = A_xmem + 1;
-    //   end
+    #0.5 
+      clk = 1'b0;
+      ififo_rd = 1'b1;
+      if(kij == 0) begin
+        x_scan_file = $fscanf(x_file,"%32b", D_xmem); 
+        WEN_xmem = 0; 
+        CEN_xmem = 0; 
+        A_xmem = A_xmem + 1;
+      end
     
-    // #0.5
-    //   clk = 1'b1;
-    for(t=0; t<row+2*col; t=t+1) begin // refer to W4S2 P.15
+    #0.5
+      clk = 1'b1;
+    for(t=0; t<row+2*col-1; t=t+1) begin // refer to W4S2 P.15
       #0.5 
         clk = 1'b0;
         ififo_rd = 1'b1;
