6af615b12e22127ab11a8f2efc4649a752f095ba
shinyuchen <b08901114@ntu.edu.tw>
2023-12-05 19:45:13 -0800

    za warudo

--- project/core_tb.v
+++ project/core_tb.v
@@ -406,12 +406,6 @@ initial begin
         clk = 1'b0;  
         execute = 1;
         if (t>0) A_xmem = A_xmem + 1;
-        if (t>=16) begin
-          ofifo_rd = 1'b1;
-          WEN_pmem = 0;
-          CEN_pmem = 0;
-          if(t>0) A_pmem = A_pmem+1;
-        end
       #0.5 clk = 1'b1;   
     end
 
@@ -454,7 +448,7 @@ initial begin
     //////// 5. OFIFO READ ////////
     // Ideally, OFIFO should be read while execution, but we have enough ofifo
     // depth so we can fetch out after execution.
-    A_pmem = len_nij * kij;
+    A_pmem = len_nij*kij;
     for(t=0; t<len_nij; t=t+1) begin
       #0.5
         clk = 1'b0;
