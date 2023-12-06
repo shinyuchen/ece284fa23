8ec8320f05dd168475dd180d3c643eb445fc1cac
shinyuchen <b08901114@ntu.edu.tw>
2023-12-05 19:41:38 -0800

    za warudo

--- project/core.v
+++ project/core.v
@@ -1,11 +1,10 @@
-module core (clk, inst, ofifo_valid, D_xmem, D_wmem, sfp_out, reset); 
+module core (clk, inst, ofifo_valid, D_xmem, sfp_out, reset); 
 
     parameter bw = 3'd4, psum_bw = 16, col = 4'd8, row = 4'd8;
 
     input clk, reset;
-    input [46:0] inst;
+    input [33:0] inst;
     input [bw*row-1:0] D_xmem;
-    input [bw*row-1:0] D_wmem;
     output ofifo_valid;
     output [psum_bw*col-1:0] sfp_out;
 
@@ -15,9 +14,7 @@ module core (clk, inst, ofifo_valid, D_xmem, D_wmem, sfp_out, reset);
     wire [psum_bw*col-1:0] ofifo_out;
     wire [psum_bw*col-1:0] sfp_in;
     wire sfp_i_valid;
-    wire [31:0] weight_SRAM_out;
-    wire [31:0] act_SRAM_out;
-    assign l0_in = (inst[2]) ? act_SRAM_out : weight_SRAM_out;
+
     assign l0_rd = (inst[3] || inst[4]);
     assign l0_wr = (inst[2] || inst[5]);
     assign ofifo_rd = inst[6];
@@ -31,11 +28,11 @@ module core (clk, inst, ofifo_valid, D_xmem, D_wmem, sfp_out, reset);
      .sfp_in(sfp_in), .sfp_i_valid(sfp_i_valid), .sfp_out(sfp_out), 
      .ofifo_o_valid(ofifo_o_valid), .ofifo_o_ready(ofifo_o_ready), 
      .ofifo_o_full(ofifo_o_full), .ofifo_out(ofifo_out), .ofifo_rd(ofifo_rd), 
-     .inst(inst[1:0])
+     .inst(inst)
     );
 
-    sram_32b_w2048 weight_SRAM (.CLK(clk), .D(D_wmem), .Q(weight_SRAM_out), .CEN(inst[19]), .WEN(inst[18]), .A(inst[17:7])); 
-    sram_32b_w2048 act_SRAM (.CLK(clk), .D(D_xmem), .Q(act_SRAM_out), .CEN(inst[46]), .WEN(inst[45]), .A(inst[44:34])); 
+    sram_32b_w2048 data_SRAM (.CLK(clk), .D(D_xmem), .Q(l0_in), .CEN(inst[19]), .WEN(inst[18]), .A(inst[17:7])); 
+
     sram_128b_w2048 psum_SRAM (.CLK(clk), .D(ofifo_out), 
                                .Q(sfp_in), 
                                .CEN(inst[32]), .WEN(inst[31]), .A(inst[30:20]));
