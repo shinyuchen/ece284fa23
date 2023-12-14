13a5e3e4f7bee4539ba97f6076583124c0dd522f
shinyuchen <b08901114@ntu.edu.tw>
2023-12-13 16:29:22 -0800

    golden experience

--- project_part5/ofifo.v
+++ project_part5/ofifo.v
@@ -53,7 +53,7 @@ module ofifo (clk, in, out, rd, wr, o_full, reset, o_ready, o_valid);
       in_buffer <= in;
       if(wr) wr_en <= {wr_en[col-2:0], 1'b1};
       else   wr_en <= {wr_en[col-2:0], 1'b0};
-      gap_wr <= (counter) ? wr_en : 0;
+      gap_wr <= (!counter) ? wr_en : 0;
       counter <= counter + 1;
     end
   end
